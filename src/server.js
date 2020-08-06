32bf5e4c04a9aed72cd2418451ad76f6f06c6d65
Pedro H. C <pedrohcm5@gmail.com>
2020-08-06 17:32:02 -0300

    updates

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const proffys = [
         name: "Giulia Xavier Pinto", avatar: "https://scontent.fsjp8-1.fna.fbcdn.net/v/t1.0-9/52638741_2227674550819080_313963200259293184_o.jpg?_nc_cat=111&_nc_sid=174925&_nc_ohc=bhw0U5AOpM0AX8wgscL&_nc_ht=scontent.fsjp8-1.fna&oh=1e16a711990231c9fce078f2df5846e1&oe=5F4ED969", 
         whatsapp: "17997668899", 
         bio: "Entusiasta das maiores obras de literatura avançada.<br><br>Apaixonada pela beleza por trás da escrita e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explicações.", 
-        subject: "Matemática", 
+        subject: "Literatura", 
         cost: "45", 
         weekday: [1], 
         time_from:[720], 
@@ -21,21 +21,36 @@ const proffys = [
     }
 ]
 
+function pageLanding(request, response) {
+    return response.render("index.html")
+}
+
+function pageStudy(request, response) {
+    return response.render("study.html", { proffys } )
+}
+
+function pageGiveClasses(request, response) {
+    return response.render("give-classes.html")
+}
+
 // Server
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
+// Nunjucks configuration
+const nunjucks = require('nunjucks')
 
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+
+server
+.use(express.static("public"))
 // Routes
-.get("/", (request, response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
-.get("/study", (request, response) => {
-    return response.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (request, response) => {
-    return response.sendFile(__dirname + "/views/give-classes.html")
-})
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 
 .listen(5000)
