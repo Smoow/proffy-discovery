6139615b6188c25f988aee497670226f9523d8fa
Pedro H. C <pedrohcm5@gmail.com>
2020-08-07 18:02:41 -0300

    updates sqlite

--- src/database/test.js
+++ src/database/test.js
@@ -1,11 +1,12 @@
 const Database = require('./db')
 const createProffy = require('./createProffy')
 
-Database.then((db) => {
+Database.then(async (db) => {
     // Insert data
 
     proffyValue = {
-        name: "Pedro Henrique Carreto", avatar: "https://avatars2.githubusercontent.com/u/37567719?s=460&u=1ee201e40649e05078a90f9366ecdfb04f89fc48&v=4", 
+        name: "Pedro Henrique Carreto", 
+        avatar: "https://avatars2.githubusercontent.com/u/37567719?s=460&u=1ee201e40649e05078a90f9366ecdfb04f89fc48&v=4", 
         whatsapp: "17997668899", 
         bio: "Entusiasta das melhores tecnologias de matemática avançada.<br><br>Apaixonado por demonstrar teoremas e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas demonstrações.", 
     }
@@ -15,7 +16,7 @@ Database.then((db) => {
         cost: "40", 
     }
 
-    classScheduleValue = [
+    classScheduleValues = [
         {
             weekday: 0, 
             time_from:720, 
@@ -28,6 +29,21 @@ Database.then((db) => {
         }
     ]
     
+    // await createProffy(db, {proffyValue, classValue, classScheduleValues})
 
     // Consult datas
+
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    // console.log(selectedProffys)
+
+    const selectedClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.* 
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffy_id)
+        WHERE classes.proffy_id = 1;
+    `)
+    console.log(selectedClassesAndProffys)
+
+    
+
 })
