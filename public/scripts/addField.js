acc93b0c7bd7c873aa7005c9bc645543c94ab1eb
Pedro H. C <pedrohcm5@gmail.com>
2020-08-11 02:14:26 -0300

    code review

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -2,11 +2,9 @@ document.querySelector("#add-time")
 .addEventListener('click', cloneField)
 
 function cloneField() {
-    // console.log("Clicou!")
     const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
     const fields = newFieldContainer.querySelectorAll('input')
 
-    //console.log(fields[0].value)
     fields.forEach(function(field) {
         field.value = ""
     })
